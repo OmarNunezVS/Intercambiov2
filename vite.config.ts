6e0d5a94c5761fbac699fb48904e35656aa1bab8
GAONUNEZ <onunez@grupoalta.com>
2025-10-19 17:44:26 -0700

    UPDATE

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/OmarNunezVS/Intercambiov2/', // 👈 Muy importante
+  base: '/Intercambiov2/', // ✅ Solo el nombre del repositorio, entre diagonales
 })
